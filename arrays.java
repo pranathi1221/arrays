983363655f1e9a0803e218efd82ad266298fe12b
pranathi1221 <pranathineo@gmail.com>
2022-05-28 21:07:11 +0530

    arrays

---
+++ arrays.java
@@ -0,0 +1,15 @@
+class Student{
+public static void main(String[] args){
+System.out.println("\nInside arrays...");
+	/*int[] scores=new int[4];
+scores[0]=90;
+scores[1]=70;
+scores[2]=80;
+scores[3]=100;*/
+   int[] scores=/*new int[]*/{90,70,80,100};
+System.out.println("Mid-term1:"+scores[0]);
+System.out.println("Mid-term2:"+scores[1]);
+System.out.println("Final:"+scores[2]);
+System.out.println("Project:"+scores[3]);
+}
+}
